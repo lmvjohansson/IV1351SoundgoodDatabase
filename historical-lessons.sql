b39f785e6ead4338090e35cc93cbf3d5c21d76a4
lmvjohansson <168458655+lmvjohansson@users.noreply.github.com>
2024-11-28 15:20:20 +0100

    Update historical-lessons.sql

--- historical-lessons.sql
+++ historical-lessons.sql
@@ -7,30 +7,30 @@ CREATE TABLE historical_lessons (
     student_email VARCHAR(500)
 );
 
--- Move individual lessons
 INSERT INTO historical_lessons (lesson_type, instrument, price, student_name, student_email)
 SELECT 'Individual', it.instrument_type, ps.price, p.name, p.email
 FROM individual_lesson il
 JOIN person_individual_lesson pil ON il.individual_lesson_id = pil.individual_lesson_id
 JOIN person p ON pil.person_id = p.person_id
 JOIN pricing_scheme ps ON il.pricing_scheme_id = ps.pricing_scheme_id
-JOIN instrument_type it ON il.instrument_type_id = it.instrument_type_id;
+JOIN instrument_type it ON il.instrument_type_id = it.instrument_type_id
+WHERE p.person_id NOT IN (SELECT person_id FROM instructor);
 
--- Move group lessons
 INSERT INTO historical_lessons (lesson_type, instrument, price, student_name, student_email)
 SELECT 'Group', it.instrument_type, ps.price, p.name, p.email
 FROM group_lesson gl
 JOIN person_group_lesson pgl ON gl.group_lesson_id = pgl.group_lesson_id
 JOIN person p ON pgl.person_id = p.person_id
 JOIN pricing_scheme ps ON gl.pricing_scheme_id = ps.pricing_scheme_id
-JOIN instrument_type it ON gl.instrument_type_id = it.instrument_type_id;
+JOIN instrument_type it ON gl.instrument_type_id = it.instrument_type_id
+WHERE p.person_id NOT IN (SELECT person_id FROM instructor);
 
--- Move ensembles
 INSERT INTO historical_lessons (lesson_type, genre, price, student_name, student_email)
 SELECT 'Ensemble', e.genre, ps.price, p.name, p.email
 FROM ensemble e
 JOIN person_ensemble pe ON e.ensemble_id = pe.ensemble_id
 JOIN person p ON pe.person_id = p.person_id
-JOIN pricing_scheme ps ON e.pricing_scheme_id = ps.pricing_scheme_id;
+JOIN pricing_scheme ps ON e.pricing_scheme_id = ps.pricing_scheme_id
+WHERE p.person_id NOT IN (SELECT person_id FROM instructor);
 
 SELECT * FROM historical_lessons;
