2abc7ae6faf1b43f98e88ae2774c37bde1ce8198
Linnea Johansson <linnea6@kth.se>
2024-12-06 23:28:00 +0100

    javadocs

--- Soundgood/src/main/java/integration/SoundgoodDAO.java
+++ Soundgood/src/main/java/integration/SoundgoodDAO.java
@@ -34,6 +34,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+
+/**
+ * Database access object to encapsulate all calls to the database.
+ */
 public class SoundgoodDAO {
     private static final String INSTRUMENT_TABLE_NAME = "instrument_inventory";
     private static final String INSTRUMENT_PK_COLUMN = "instrument_inventory_id";
@@ -52,6 +56,10 @@ public class SoundgoodDAO {
     private Connection connection;
     private PreparedStatement findInstrumentByType;
 
+    /**
+     * Creates new database access object with connection to the database and prepared statements
+     * @throws SoundgoodDBException if unable to connect to database or unable to parse prepared statements
+     */
     public SoundgoodDAO() throws SoundgoodDBException {
         try {
             connectToSoundgoodDB();
@@ -61,6 +69,10 @@ public class SoundgoodDAO {
         }
     }
 
+    /**
+     * Commits the current transaction
+     * @throws SoundgoodDBException if unable to commit the current transaction
+     */
     public void commit() throws SoundgoodDBException {
         try {
             connection.commit();
@@ -69,6 +81,12 @@ public class SoundgoodDAO {
         }
     }
 
+    /**
+     * Method to find instruments by a given type that are not currently rented out
+     * @param instrumentType
+     * @return list of InstrumentDTOs representing the instruments
+     * @throws SoundgoodDBException if unable to retrieve the instruments
+     */
     public ArrayList<InstrumentDTO> findInstrumentByType(String instrumentType) throws SoundgoodDBException {
         PreparedStatement stmtToExecute = findInstrumentByType;
         String failureMsg = "Could not search for instruments.";
@@ -110,13 +128,9 @@ public class SoundgoodDAO {
     private void prepareStatements() throws SQLException {
         findInstrumentByType = connection.prepareStatement(
                 " SELECT i." + INSTRUMENT_PK_COLUMN + ", ? as " + INSTRUMENT_TYPE_TABLE_NAME + " , i." + INSTRUMENT_BRAND_COLUMN + ", i." + INSTRUMENT_PRICE_COLUMN +
-                        " FROM " + INSTRUMENT_TABLE_NAME + " i" +
-                        " WHERE i." + INSTRUMENT_TYPE_COLUMN + " = (SELECT " + INSTRUMENT_TYPE_ID_COLUMN +
-                        " FROM " + INSTRUMENT_TYPE_TABLE_NAME +
-                        " WHERE instrument_type = ?)" +
-                        " AND i." + INSTRUMENT_PK_COLUMN + " NOT IN (SELECT " + LEASE_INSTRUMENT_ID_COLUMN +
-                        " FROM " + LEASE_TABLE_NAME +
-                        " WHERE " + LEASE_EXPIRATION_DATE_COLUMN + " > CURRENT_DATE)"
+                        " FROM " + INSTRUMENT_TABLE_NAME + " i" + " WHERE i." + INSTRUMENT_TYPE_COLUMN + " = (SELECT " + INSTRUMENT_TYPE_ID_COLUMN +
+                        " FROM " + INSTRUMENT_TYPE_TABLE_NAME + " WHERE instrument_type = ?)" + " AND i." + INSTRUMENT_PK_COLUMN + " NOT IN (SELECT " + LEASE_INSTRUMENT_ID_COLUMN +
+                        " FROM " + LEASE_TABLE_NAME + " WHERE " + LEASE_EXPIRATION_DATE_COLUMN + " > CURRENT_DATE)"
         );
     }
 
