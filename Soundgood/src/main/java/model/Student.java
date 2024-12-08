438e2278e9e13892f741f0f8b1a1f2d7c2b17912
J-Segl <jesper.wannman@gmail.com>
2024-12-08 12:32:28 +0100

    Update Student.java
    Completed student class.

--- Soundgood/src/main/java/model/Student.java
+++ Soundgood/src/main/java/model/Student.java
@@ -1,4 +1,47 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Student {
+    private StudentDTO student;
+    private ArrayList<LeaseDTO> activeLeases;
+
+    public Student(StudentDTO student) {
+        this.student = student;
+        this.activeLeases = new ArrayList<>();
+    }
+
+    public void setLeases(ArrayList<LeaseDTO> leases) {
+        this.activeLeases = leases;
+    }
+
+    public boolean isRentalAllowed() {
+        return activeLeases.size() < 2;
+    }
+
+    public Lease findLease(StudentAndInstrumentDTO dto) {
+        for (LeaseDTO lease : activeLeases) {
+            if (lease.getInstrumentId() == dto.getInstrumentId() &&
+                lease.getStudentId() == dto.getStudentId()) {
+                return new Lease(lease);
+            }
+        }
+        return null;
+    }
+
+    public StudentDTO getStudent() {
+        return student;
+    }
+
+    public void setStudent(StudentDTO student) {
+        this.student = student;
+    }
+
+    public ArrayList<LeaseDTO> getActiveLeases() {
+        return activeLeases;
+    }
+
+    public void setActiveLeases(ArrayList<LeaseDTO> activeLeases) {
+        this.activeLeases = activeLeases;
+    }
 }
